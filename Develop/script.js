41c3c327796b343eca17174d7429231fa61a6fa9
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-07-24 21:34:30 -0500

    Updated js and html

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+const searchSectionEl = document.querySelector("search-section")
+const forecastSectionEl = document.querySelector("forecast-section")
+const searchButtonEl = document.querySelector("search-button")
+const clearHistoryEl = document.querySelector("clear-history")
+const forecastItemEl = document.querySelector("forecast-item")
+const searchHistoryListEl = document.querySelector("search-history-list")
