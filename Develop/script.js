854eeff536a05ffc645a27edf6243d54682666f9
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-07-26 17:39:16 -0500

    Updated Project

--- Develop/script.js
+++
@@ -1,8 +0,0 @@
-const searchSectionEl = document.querySelector("search-section")
-const forecastSectionEl = document.querySelector("forecast-section")
-const searchButtonEl = document.querySelector("search-button")
-const clearHistoryEl = document.querySelector("clear-history")
-const forecastItemEl = document.querySelector("forecast-item")
-const searchHistoryListEl = document.querySelector("search-history-list")
-
-
